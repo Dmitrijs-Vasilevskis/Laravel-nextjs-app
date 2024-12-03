e75dfbc4de2b2b168f5d8ffdc53b1037f066b01b
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2024-12-03 23:56:25 +0200

    Auth hook code style

--- frontend/src/app/hooks/auth.js
+++ frontend/src/app/hooks/auth.js
@@ -23,100 +23,105 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         router.push("/verify-email");
       })
   );
+  
   const register = async ({ setErrors, ...props }) => {
-    await csrf();
+      await csrf();
 
-    setErrors([]);
+      setErrors([]);
 
-    axios
-      .post("/register", props)
-      .then(() => mutate())
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
+      axios
+          .post("/register", props)
+          .then(() => mutate())
+          .catch((error) => {
+              if (error.response.status !== 422) throw error;
 
-        setErrors(error.response.data.errors);
-      });
+              setErrors(error.response.data.errors);
+          });
   };
 
   const login = async ({ setErrors, setStatus, ...props }) => {
-    await csrf();
+      await csrf();
 
-    setErrors([]);
-    setStatus(null);
+      setErrors([]);
+      setStatus(null);
 
-    return axios
-      .post("/login", props)
-      .then(() => mutate())
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
+      return axios
+          .post("/login", props)
+          .then(() => mutate())
+          .catch((error) => {
+              if (error.response.status !== 422) throw error;
 
-        setErrors(error.response.data.errors);
-      });
+              setErrors(error.response.data.errors);
+          });
   };
 
   const forgotPassword = async ({ setErrors, setStatus, email }) => {
-    await csrf();
+      await csrf();
 
-    setErrors([]);
-    setStatus(null);
+      setErrors([]);
+      setStatus(null);
 
-    axios
-      .post("/forgot-password", { email })
-      .then((response) => setStatus(response.data.status))
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
+      axios
+          .post("/forgot-password", { email })
+          .then((response) => setStatus(response.data.status))
+          .catch((error) => {
+              if (error.response.status !== 422) throw error;
 
-        setErrors(error.response.data.errors);
-      });
+              setErrors(error.response.data.errors);
+          });
   };
 
   const resetPassword = async ({ setErrors, setStatus, ...props }) => {
-    await csrf();
+      await csrf();
 
-    setErrors([]);
-    setStatus(null);
+      setErrors([]);
+      setStatus(null);
 
-    axios
-      .post("/reset-password", { token: params.token, ...props })
-      .then((response) =>
-        router.push("/login?reset=" + btoa(response.data.status))
-      )
-      .catch((error) => {
-        if (error.response.status !== 422) throw error;
+      axios
+          .post("/reset-password", { token: params.token, ...props })
+          .then((response) =>
+              router.push("/login?reset=" + btoa(response.data.status))
+          )
+          .catch((error) => {
+              if (error.response.status !== 422) throw error;
 
-        setErrors(error.response.data.errors);
-      });
+              setErrors(error.response.data.errors);
+          });
   };
 
   const resendEmailVerification = ({ setStatus }) => {
-    axios
-      .post("/email/verification-notification")
-      .then((response) => setStatus(response.data.status));
+      axios
+          .post("/email/verification-notification")
+          .then((response) => setStatus(response.data.status));
   };
 
   const logout = async () => {
-    if (!error) {
-      await axios.post("/logout").then(() => mutate());
-    }
+      if (!error) {
+          await axios.post("/logout").then(() => mutate());
+      }
 
-    window.location.pathname = "/";
+      window.location.pathname = "/";
   };
 
   useEffect(() => {
-    if (middleware === "guest" && redirectIfAuthenticated && user)
-      router.push(redirectIfAuthenticated);
-    if (window.location.pathname === "/verify-email" && user?.email_verified_at)
-      router.push(redirectIfAuthenticated);
-    if (middleware === "auth" && error) logout();
+      if (middleware === "guest" && redirectIfAuthenticated && user)
+          router.push(redirectIfAuthenticated);
+      if (
+          window.location.pathname === "/verify-email" &&
+          user?.email_verified_at
+      )
+          router.push(redirectIfAuthenticated);
+      if (middleware === "auth" && error) logout();
   }, [user, error]);
 
   return {
-    user,
-    register,
-    login,
-    forgotPassword,
-    resetPassword,
-    resendEmailVerification,
-    logout,
+      user,
+      register,
+      login,
+      forgotPassword,
+      resetPassword,
+      resendEmailVerification,
+      logout,
+      isLoggedIn: !!user,
   };
 };
